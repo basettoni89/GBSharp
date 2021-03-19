57268123bbdf7f02a762a4c9050a708ec1ac9b91
Davide Mortara <dmortara@alteanet.it>
2021-03-19 09:01:44 +0100

    Added new GBEmu.GUI project to test out SDL2

---
+++ GBEmu/GBEmu.GUI/Program.cs
@@ -0,0 +1,79 @@
+﻿using SDL2;
+using System;
+using System.Runtime.InteropServices;
+
+namespace GBEmu.GUI
+{
+    class Program
+    {
+        static IntPtr renderer;
+        static IntPtr texture;
+
+        static byte[,,] pixels = new byte[144, 160, 3];
+
+        static void Main(string[] args)
+        {
+            SDL.SDL_Init(SDL.SDL_INIT_EVERYTHING);
+
+            var window = SDL.SDL_CreateWindow("GBEmu",
+                SDL.SDL_WINDOWPOS_CENTERED,
+                SDL.SDL_WINDOWPOS_CENTERED,
+                160,
+                144,
+                SDL.SDL_WindowFlags.SDL_WINDOW_SHOWN);
+
+            renderer = SDL.SDL_CreateRenderer(window, -1, SDL.SDL_RendererFlags.SDL_RENDERER_ACCELERATED);
+
+            texture = SDL.SDL_CreateTexture(renderer,
+                SDL.SDL_PIXELFORMAT_RGB24,
+                (int)SDL.SDL_TextureAccess.SDL_TEXTUREACCESS_STREAMING,
+                160, 144);
+
+            for (int x = 0; x < 144; x++)
+            {
+                for (int y = 0; y < 160; y++)
+                {
+                    pixels[x, y, 0] = 0x00;
+                    pixels[x, y, 1] = 0xDD;
+                    pixels[x, y, 2] = 0xDD;
+                }
+            }
+
+
+            bool quit = false;
+
+            while (!quit)
+            {
+                while(SDL.SDL_PollEvent(out SDL.SDL_Event e) != 0)
+                {
+                    switch (e.type)
+                    {
+                        case SDL.SDL_EventType.SDL_QUIT:
+                            quit = true;
+                            break;
+                    }
+                }
+
+                Render();
+            }
+
+            SDL.SDL_Quit();
+        }
+
+        static void Render()
+        {
+            GCHandle handle = GCHandle.Alloc(pixels, GCHandleType.Pinned);
+            IntPtr pixelsPtr = handle.AddrOfPinnedObject();
+
+            SDL.SDL_UpdateTexture(texture, IntPtr.Zero, pixelsPtr, 160 * sizeof(byte) * 3);
+
+            SDL.SDL_RenderClear(renderer);
+            SDL.SDL_RenderCopy(renderer, texture, IntPtr.Zero, IntPtr.Zero);
+            SDL.SDL_RenderPresent(renderer);
+
+            SDL.SDL_RenderClear(renderer);
+
+            handle.Free();
+        }
+    }
+}
--- GBEmu/GBEmu.Win/Window.cs
+++ GBEmu/GBEmu.Win/Window.cs
@@ -1,9 +1,9 @@
 ﻿
-using OpenTK.Graphics.OpenGL4;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.GraphicsLibraryFramework;
 using OpenTK.Windowing.Desktop;
 using System.Diagnostics;
+using OpenTK.Graphics.OpenGL;
 
 namespace GBEmu.Win
 {
@@ -37,78 +37,111 @@ namespace GBEmu.Win
 
     public class Window : GameWindow
     {
-        private readonly uint[] _indices =
-        {
-            0, 1, 3, // The first triangle will be the bottom-right half of the triangle
-            1, 2, 3  // Then the second will be the top-right half of the triangle
-        };
+        //private readonly uint[] _indices =
+        //{
+        //    0, 1, 3, // The first triangle will be the bottom-right half of the triangle
+        //    1, 2, 3  // Then the second will be the top-right half of the triangle
+        //};
 
-        private int _vertexBufferObject;
+        //private int _vertexBufferObject;
 
-        private int _vertexArrayObject;
+        //private int _vertexArrayObject;
 
-        private Shader _shader;
+        //private Shader _shader;
 
-        // Add a handle for the EBO
-        private int _elementBufferObject;
+        //// Add a handle for the EBO
+        //private int _elementBufferObject;
 
         private readonly Color bgColor = new Color(0x9B, 0xBC, 0x0F, 0xFF);
         private readonly Color fgColor = new Color(0x30, 0x62, 0x30, 0xFF);
 
+        private readonly byte[,,] frameBuffer;
+
         public Window(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
             : base(gameWindowSettings, nativeWindowSettings)
         {
+            frameBuffer = new byte[Size.X, Size.Y, 3];
+
+            for (int x = 0; x < Size.X; x++)
+            {
+                for(int y = 0; y < Size.Y; y++)
+                {
+                    frameBuffer[x, y, 0] = 0;
+                    frameBuffer[x, y, 1] = 0;
+                    frameBuffer[x, y, 2] = 0;
+                }
+            }
         }
 
         protected override void OnLoad()
         {
-            var normalizedBG = bgColor.GetNormalizedColor();
-            GL.ClearColor(normalizedBG[0], normalizedBG[1], normalizedBG[2], normalizedBG[3]);
+            //var normalizedBG = bgColor.GetNormalizedColor();
+            //GL.ClearColor(normalizedBG[0], normalizedBG[1], normalizedBG[2], normalizedBG[3]);
 
-            var normalizedFG = fgColor.GetNormalizedColor();
-            float[] _vertices =
-            {
-                 0.5f,  0.5f, 0.0f, normalizedFG[0], normalizedFG[1], normalizedFG[2], // top right
-                 0.5f, -0.5f, 0.0f, normalizedFG[0], normalizedFG[1], normalizedFG[2], // bottom right
-                -0.5f, -0.5f, 0.0f, normalizedFG[0], normalizedFG[1], normalizedFG[2], // bottom left
-                -0.5f,  0.5f, 0.0f, normalizedFG[0], normalizedFG[1], normalizedFG[2], // top left
-            };
+            //var normalizedFG = fgColor.GetNormalizedColor();
+            //float[] _vertices =
+            //{
+            //     0.5f,  0.5f, 0.0f, normalizedFG[0], normalizedFG[1], normalizedFG[2], // top right
+            //     0.5f, -0.5f, 0.0f, normalizedFG[0], normalizedFG[1], normalizedFG[2], // bottom right
+            //    -0.5f, -0.5f, 0.0f, normalizedFG[0], normalizedFG[1], normalizedFG[2], // bottom left
+            //    -0.5f,  0.5f, 0.0f, normalizedFG[0], normalizedFG[1], normalizedFG[2], // top left
+            //};
 
-            _vertexBufferObject = GL.GenBuffer();
-            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
-            GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+            //_vertexBufferObject = GL.GenBuffer();
+            //GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+            //GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
 
-            _vertexArrayObject = GL.GenVertexArray();
-            GL.BindVertexArray(_vertexArrayObject);
+            //_vertexArrayObject = GL.GenVertexArray();
+            //GL.BindVertexArray(_vertexArrayObject);
 
-            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 6 * sizeof(float), 0);
-            GL.EnableVertexAttribArray(0);
+            //GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 6 * sizeof(float), 0);
+            //GL.EnableVertexAttribArray(0);
 
-            GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 6 * sizeof(float), 3 * sizeof(float));
-            GL.EnableVertexAttribArray(1);
+            //GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 6 * sizeof(float), 3 * sizeof(float));
+            //GL.EnableVertexAttribArray(1);
 
-            GL.GetInteger(GetPName.MaxVertexAttribs, out int maxAttributeCount);
-            Debug.WriteLine($"Maximum number of vertex attributes supported: {maxAttributeCount}");
+            //GL.GetInteger(GetPName.MaxVertexAttribs, out int maxAttributeCount);
+            //Debug.WriteLine($"Maximum number of vertex attributes supported: {maxAttributeCount}");
 
-            _elementBufferObject = GL.GenBuffer();
-            GL.BindBuffer(BufferTarget.ElementArrayBuffer, _elementBufferObject);
-            GL.BufferData(BufferTarget.ElementArrayBuffer, _indices.Length * sizeof(uint), _indices, BufferUsageHint.StaticDraw);
+            //_elementBufferObject = GL.GenBuffer();
+            //GL.BindBuffer(BufferTarget.ElementArrayBuffer, _elementBufferObject);
+            //GL.BufferData(BufferTarget.ElementArrayBuffer, _indices.Length * sizeof(uint), _indices, BufferUsageHint.StaticDraw);
 
-            _shader = new Shader("Shaders/shader.vert", "Shaders/shader.frag");
-            _shader.Use();
+            //_shader = new Shader("Shaders/shader.vert", "Shaders/shader.frag");
+            //_shader.Use();
+
+            GL.Viewport(0, 0, Size.X, Size.Y);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+            GL.Ortho(0, Size.X, Size.Y, 0, -1.0f, 1.0f);
+            GL.ClearColor(0, 1, 0, 1);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GL.ShadeModel(ShadingModel.Flat);
+
+            GL.Enable(EnableCap.Texture2D);
+            GL.Disable(EnableCap.DepthTest);
+            GL.Disable(EnableCap.CullFace);
+            GL.Disable(EnableCap.Dither);
+            GL.Disable(EnableCap.Blend);
 
             base.OnLoad();
         }
 
         protected override void OnRenderFrame(FrameEventArgs e)
         {
-            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
-            _shader.Use();
+            GL.LoadIdentity();
+            GL.RasterPos2(-1, 1);
+            GL.PixelZoom(1, -1);
 
-            GL.BindVertexArray(_vertexArrayObject);
+            GL.DrawPixels(Size.X, Size.Y, PixelFormat.Rgb, PixelType.Byte, frameBuffer);
 
-            GL.DrawElements(PrimitiveType.Triangles, _indices.Length, DrawElementsType.UnsignedInt, 0);
+            //_shader.Use();
+
+            //GL.BindVertexArray(_vertexArrayObject);
+
+            //GL.DrawElements(PrimitiveType.Triangles, _indices.Length, DrawElementsType.UnsignedInt, 0);
 
             SwapBuffers();
 
@@ -140,10 +173,10 @@ namespace GBEmu.Win
             GL.BindVertexArray(0);
             GL.UseProgram(0);
 
-            GL.DeleteBuffer(_vertexBufferObject);
-            GL.DeleteBuffer(_elementBufferObject);
-            GL.DeleteVertexArray(_vertexArrayObject);
-            GL.DeleteProgram(_shader.Handle);
+            //GL.DeleteBuffer(_vertexBufferObject);
+            //GL.DeleteBuffer(_elementBufferObject);
+            //GL.DeleteVertexArray(_vertexArrayObject);
+            //GL.DeleteProgram(_shader.Handle);
 
             base.OnUnload();
         }
